24ea12040cc2b15b35ab1b31065f75bf37ad8d74
Natiums <devnatanael@gmail.com>
2020-06-11 16:51:09 -0400

    =changes

--- gather.php
+++ gather.php
@@ -3,7 +3,7 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-$gather = $response->gather(['numDigits' => '1', 'action' => '/twilio/process_gather.php',
+$gather = $response->gather(['numDigits' => '1', 'action' => 'process_gather.php',
     'method' => 'GET']);
 $gather->play('https://fast-cove-95462.herokuapp.com/menu.mp3', array('loop' => 2));
 //$gather->say('Gracias por llamar, por favor al escuchar el menu presiona el digito deseado luego de la tecla de numero. Para escuchar la cancion Mi corazon encantado presiona 1, para escuchar la cancion Angeles Fuimos presiona 2',array("voice" => "alice", "language" => "es-ES"));

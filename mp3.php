9992e80a231f26fafdda8ab590997d7cd85236d9
Natiums <devnatanael@gmail.com>
2020-06-12 21:00:00 -0400

    =making ratio station change

--- mp3.php
+++ mp3.php
@@ -5,8 +5,9 @@ $mp3path = "https://playerservices.streamtheworld.com/api/livestream-redirect/97
 $file = 'https://playerservices.streamtheworld.com/api/livestream-redirect/977_HITSAAC_SC';
 
 header("Content-Type: audio/mpeg");
+echo file_get_contents($file)
 //header("Content-Transfer-Encoding: binary");
 //header("Content-Disposition: attachment; filename=".$file);
 //header("Content-Length: ".filesize($file));
 
-readfile($file);
+//readfile(file_get_contents($file));

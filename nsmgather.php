4a86026c87853ca3c6627a8474a747ccee47f147
Natiums <devnatanael@gmail.com>
2020-06-13 14:47:18 -0400

    =changes

--- nsmgather.php
+++ nsmgather.php
@@ -6,7 +6,7 @@ $response = new VoiceResponse();
 $gather = $response->gather([
 	'action' => '/nsmgather_process.php',
 	'numDigits' => 1,
-    'method' => 'GET'
+    'method' => 'POST'
 ]);
 $gather->say('For sales, press 1. For support, press 2.');
 $response->say('We didn\'t receive any input. Goodbye!');
